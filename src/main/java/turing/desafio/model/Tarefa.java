5fb49f23f1ddce976aa1c9fd62627b5b7c493f62
Lucas Moura <felipelucasmoura350@gmail.com>
2026-04-20 15:50:04 -0300

    feat: Criando Entidade Tarefa

---
+++ src/main/java/turing/desafio/model/Tarefa.java
@@ -0,0 +1,27 @@
+package turing.desafio.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String descricao;
+    private boolean concluido;
+    private LocalDate data;
+}
